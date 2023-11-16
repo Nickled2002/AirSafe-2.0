67c3220b074ce869306b9bba69e5e15e93cf14a7
Nickled2002 <64671430+Nickled2002@users.noreply.github.com>
2023-11-16 14:19:01 +0000

    Added dependencies plus return all wgpu backend in the machine

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    let instances = wgpu::Instance::new(wgpu::Backends::all());
+    for adapter in instances.enumerate_adapters(wgpu::Backends::all()){
+        println!("{:?}",adapter.get_info())
+    }
 }
