55adfabbcfd8b3aa7574eaf98e0658840a7c3059
Nickled2002 <64671430+Nickled2002@users.noreply.github.com>
2023-11-16 15:47:40 +0000

    Made window event adding wgsl shaders file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,27 @@
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::Window,
+};
 fn main() {
-    let instances = wgpu::Instance::new(wgpu::Backends::all());
-    for adapter in instances.enumerate_adapters(wgpu::Backends::all()){
-        println!("{:?}",adapter.get_info())
-    }
+   //========================Check backend====================
+   //let instances = wgpu::Instance::new(wgpu::Backends::all());
+   //for adapter in instances.enumerate_adapters(wgpu::Backends::all()){
+   //     println!("{:?}",adapter.get_info())
+   // }
+    let event_loop: EventLoop<()> = EventLoop::new();
+    let window: Window = Window::new(&event_loop).unwrap();
+    window.set_title("my_window");
+    env_logger::init();
+
+    event_loop.run(move |event: Event<()>, _,control_flow: &mut ControlFlow|{
+        *control_flow = ControlFlow::Wait;
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                 ..
+            } => *control_flow = ControlFlow::Exit,
+            _=>{}
+        }
+    });
 }
