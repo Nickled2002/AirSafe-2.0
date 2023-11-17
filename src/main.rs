394bf37aa83244448875e2da8cab0a4f11cf2541
Nickled2002 <64671430+Nickled2002@users.noreply.github.com>
2023-11-17 14:46:21 +0000

    Triangle with colors fixed

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,24 @@ use winit::{
     event_loop::{ControlFlow, EventLoop},
     window::Window,
 };
-use std::borrow::Cow;
-use std::future::Future;
-use winit::event::VirtualKeyCode::N;
+//use std::future::Future;
+//use winit::event::VirtualKeyCode::N;
 
-pub async fn run(event_loop: EventLoop<()>, window: Window){
+pub async fn run(event_loop: EventLoop<()>, window: &Window) {
     let size = window.inner_size();
-    let instance = wgpu::Instance::new(wgpu::Backends::VULKAN);
-    let surface = unsafe { instance.create_surface(&window)};
+    let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+        backends: wgpu::Backends::DX12,
+        dx12_shader_compiler: Default::default(),
+    });
+    let surface = unsafe { instance.create_surface(&window) }.unwrap();
     let adapter = instance
         .request_adapter(&wgpu::RequestAdapterOptions {
             power_preference: wgpu::PowerPreference::default(),
             compatible_surface: Some(&surface),
-            force_fallback_adapter: false
+            force_fallback_adapter: false,
         })
         .await
-        .expect("Failed to find an apropriate adapter");
+        .expect("Failed to find an appropriate adapter");
 
     let (device, queue) = adapter
         .request_device(
@@ -32,39 +34,51 @@ pub async fn run(event_loop: EventLoop<()>, window: Window){
         .await
         .expect("Failed to create device");
 
-    let format = surface.get_preferred_format(&adapter).unwrap();
+    let surface_caps = surface.get_capabilities(&adapter);
+    let format = surface_caps.formats[0];
+
     let mut config = wgpu::SurfaceConfiguration {
         usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-        format: format,
+        format,
         width: size.width,
         height: size.height,
-        present_mode: wgpu::PresentMode::Mailbox,
+        present_mode: wgpu::PresentMode::Fifo,
+        alpha_mode:surface_caps.alpha_modes[0],
+        view_formats: vec![],
     };
     surface.configure(&device, &config);
 
-    let shader = device.create_shader_module(&wgpu::ShaderModuleDescriptor{
+    // Load the shaders from disk
+    let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
         label: None,
-        source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(include_str!("shader.wgsl"))),
+        source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
     });
-    //Placegolder we dont use gpu buffer
-    let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor{
+
+    let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
         label: None,
-        bind_group_layouts: &[],
+        bind_group_layouts: &[],//Placeholder we dont use gpu buffer
         push_constant_ranges: &[],
     });
     //Describes the actions the GPU will perform when acting on a set of data
-    let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor{
+    let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
         label: None,
         layout: Some(&pipeline_layout),
-        vertex: wgpu::VertexState{
+        vertex: wgpu::VertexState {
             module: &shader,
             entry_point: "vs_main",
             buffers: &[],
         },
-        fragment: Some(wgpu::FragmentState{
+        fragment: Some(wgpu::FragmentState {
             module: &shader,
             entry_point: "fs_main",
-            targets: &[format.into()],
+            targets: &[Some(wgpu::ColorTargetState {
+                format,
+                blend: Some(wgpu::BlendState {
+                    color: wgpu::BlendComponent::REPLACE,
+                    alpha: wgpu::BlendComponent::REPLACE,
+                }),
+                write_mask: wgpu::ColorWrites::ALL,
+            })],
         }),
         primitive: wgpu::PrimitiveState::default(),
         depth_stencil: None,
@@ -72,8 +86,8 @@ pub async fn run(event_loop: EventLoop<()>, window: Window){
         multiview: None,
     });
 
-    event_loop.run(move |event,_, control_flow| {
-        let _ =(&instance, &adapter, &shader, &pipeline_layout);
+    event_loop.run(move |event, _, control_flow| {
+        let _ = (&instance, &adapter, &shader, &pipeline_layout);
         *control_flow = ControlFlow::Wait;
 
         match event {
@@ -81,7 +95,8 @@ pub async fn run(event_loop: EventLoop<()>, window: Window){
                 event: WindowEvent::Resized(size),
                 ..
             } => {
-                //Recreate surface with the new size
+                // Recreate the surface with the new size
+                instance.poll_all(true);
                 config.width = size.width;
                 config.height = size.height;
                 surface.configure(&device, &config);
@@ -89,19 +104,19 @@ pub async fn run(event_loop: EventLoop<()>, window: Window){
             Event::RedrawRequested(_) => {
                 let frame = surface.get_current_texture().unwrap();
                 let view = frame.texture.create_view(&wgpu::TextureViewDescriptor::default());
-                let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {label: None});
+                let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
                 {
-                    let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor{
+                    let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
                         label: None,
-                        color_attachments: &[wgpu::RenderPassColorAttachment {
+                        color_attachments: &[Some(wgpu::RenderPassColorAttachment {
                             view: &view,
                             resolve_target: None,
-                            ops: wgpu::Operations{
+                            ops: wgpu::Operations {
                                 load: wgpu::LoadOp::Clear(wgpu::Color {r: 0.05, g:0.062, b:0.08, a:1.0}),
                                 store: true,
                             },
-                        }],
-                        depth_stencil_attachment: None, //set render for 3d stuff
+                        })],
+                        depth_stencil_attachment: None,
                     });
                     rpass.set_pipeline(&render_pipeline);
                     rpass.draw(0..3, 0..1);
@@ -111,26 +126,25 @@ pub async fn run(event_loop: EventLoop<()>, window: Window){
                 frame.present();
             }
             Event::WindowEvent {
-                event:WindowEvent::CloseRequested,
+                event: WindowEvent::CloseRequested,
                 ..
             } => *control_flow = ControlFlow::Exit,
             _ => {}
         }
     });
-
 }
+
 fn main() {
-   //========================Check backend EP1====================
-   //let instances = wgpu::Instance::new(wgpu::Backends::all());
-   //for adapter in instances.enumerate_adapters(wgpu::Backends::all()){
-   //     println!("{:?}",adapter.get_info())
-   // }
+    //========================Check backend EP1====================
+    //let instances = wgpu::Instance::new(wgpu::Backends::all());
+    //for adapter in instances.enumerate_adapters(wgpu::Backends::all()){
+    //     println!("{:?}",adapter.get_info())
+    // }
     //====================Window Generation EP2=================
     //let event_loop: EventLoop<()> = EventLoop::new();
     //let window: Window = Window::new(&event_loop).unwrap();
     //window.set_title("my_window");
     //env_logger::init();
-
     //event_loop.run(move |event: Event<()>, _,control_flow: &mut ControlFlow|{
     //   *control_flow = ControlFlow::Wait;
     //    match event {
@@ -141,11 +155,9 @@ fn main() {
     //        _=>{}
     //    }
     //});
-
     let event_loop = EventLoop::new();
-    let window =winit::window::Window::new(&event_loop).unwrap();
+    let window = winit::window::Window::new(&event_loop).unwrap();
     window.set_title("wgpu03");
     env_logger::init();
-    pollster::block_on(run(event_loop,window));
-
+    pollster::block_on( run(event_loop, &window));
 }
