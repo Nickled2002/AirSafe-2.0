b56c9026914abfea88fe144888d46f59a23e2dcc
Nickled2002 <64671430+Nickled2002@users.noreply.github.com>
2023-11-18 16:24:24 +0000

    gpu buffer basics

--- src/main.rs
+++ src/main.rs
@@ -1,163 +1,288 @@
+use std::{iter, mem};
+
+use bytemuck::{Pod, Zeroable};
+use wgpu::util::DeviceExt;
 use winit::{
-    event::{Event, WindowEvent},
+    event::*,
     event_loop::{ControlFlow, EventLoop},
-    window::Window,
+    window::{Window, WindowBuilder},
 };
-//use std::future::Future;
-//use winit::event::VirtualKeyCode::N;
 
-pub async fn run(event_loop: EventLoop<()>, window: &Window) {
-    let size = window.inner_size();
-    let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
-        backends: wgpu::Backends::VULKAN,
-        dx12_shader_compiler: Default::default(),
-    });
-    let surface = unsafe { instance.create_surface(&window) }.unwrap();
-    let adapter = instance
-        .request_adapter(&wgpu::RequestAdapterOptions {
-            power_preference: wgpu::PowerPreference::default(),
-            compatible_surface: Some(&surface),
-            force_fallback_adapter: false,
-        })
-        .await
-        .expect("Failed to find an appropriate adapter");
+#[repr(C)]
+#[derive(Copy, Clone, Debug, Pod, Zeroable)]
+//use gpu buffer to store the vertex and color data
+struct Vertex {// define vertex structure
+    position: [f32; 2],
+    color: [f32; 3],
+}
 
-    let (device, queue) = adapter
-        .request_device(
-            &wgpu::DeviceDescriptor {
-                label: None,
-                features: wgpu::Features::empty(),
-                limits: wgpu::Limits::default(),
-            },
-            None,
-        )
-        .await
-        .expect("Failed to create device");
-
-    let surface_caps = surface.get_capabilities(&adapter);
-    let format = surface_caps.formats[0];
-
-    let mut config = wgpu::SurfaceConfiguration {
-        usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-        format,
-        width: size.width,
-        height: size.height,
-        present_mode: wgpu::PresentMode::Fifo,
-        alpha_mode:surface_caps.alpha_modes[0],
-        view_formats: vec![],
-    };
-    surface.configure(&device, &config);
-
-    // Load the shaders from disk
-    let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
-        label: None,
-        source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
-    });
-
-    let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-        label: None,
-        bind_group_layouts: &[],//Placeholder we dont use gpu buffer
-        push_constant_ranges: &[],
-    });
-    //Describes the actions the GPU will perform when acting on a set of data
-    let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-        label: None,
-        layout: Some(&pipeline_layout),
-        vertex: wgpu::VertexState {
-            module: &shader,
-            entry_point: "vs_main",
-            buffers: &[],
-        },
-        fragment: Some(wgpu::FragmentState {
-            module: &shader,
-            entry_point: "fs_main",
-            targets: &[Some(wgpu::ColorTargetState {
-                format,
-                blend: Some(wgpu::BlendState {
-                    color: wgpu::BlendComponent::REPLACE,
-                    alpha: wgpu::BlendComponent::REPLACE,
-                }),
-                write_mask: wgpu::ColorWrites::ALL,
-            })],
-        }),
-        primitive: wgpu::PrimitiveState::default(),
-        depth_stencil: None,
-        multisample: wgpu::MultisampleState::default(),
-        multiview: None,
-    });
-
-    event_loop.run(move |event, _, control_flow| {
-        let _ = (&instance, &adapter, &shader, &pipeline_layout);
-        *control_flow = ControlFlow::Wait;
-
-        match event {
-            Event::WindowEvent {
-                event: WindowEvent::Resized(size),
-                ..
-            } => {
-                // Recreate the surface with the new size
-                instance.poll_all(true);
-                config.width = size.width;
-                config.height = size.height;
-                surface.configure(&device, &config);
-            }
-            Event::RedrawRequested(_) => {
-                let frame = surface.get_current_texture().unwrap();
-                let view = frame.texture.create_view(&wgpu::TextureViewDescriptor::default());
-                let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
-                {
-                    let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                        label: None,
-                        color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                            view: &view,
-                            resolve_target: None,
-                            ops: wgpu::Operations {
-                                load: wgpu::LoadOp::Clear(wgpu::Color {r: 0.05, g:0.062, b:0.08, a:1.0}),
-                                store: true,
-                            },
-                        })],
-                        depth_stencil_attachment: None,
-                    });
-                    rpass.set_pipeline(&render_pipeline);
-                    rpass.draw(0..3, 0..1);
-                }
-
-                queue.submit(Some(encoder.finish()));
-                frame.present();
-            }
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                ..
-            } => *control_flow = ControlFlow::Exit,
-            _ => {}
+impl Vertex {
+    const ATTRIBUTES: [wgpu::VertexAttribute; 2] =
+        wgpu::vertex_attr_array![0 => Float32x2, 1 => Float32x3];//vertex_attr_array: used to define static method called
+    fn desc<'a>() -> wgpu::VertexBufferLayout<'a> {//description function and returns vertex buffer layout use it to tell the render pipeline how to read the data from the gp buffer
+        wgpu::VertexBufferLayout {//use it to map the data to shader code
+            array_stride: mem::size_of::<Vertex>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,// how ofthen it should move to the next vortex can be set to instance
+            attributes: &Self::ATTRIBUTES,//two dimensional array defined in line 21 with the location in the shader and the format
         }
-    });
+    }
+}
+
+const VERTICES: &[Vertex] = &[//set vertex data and color data for the square
+    Vertex {
+        // vertex a
+        position: [-0.5, -0.5],
+        color: [1.0, 0.0, 0.0],
+    },
+    Vertex {
+        // vertex b
+        position: [0.5, -0.5],
+        color: [0.0, 1.0, 0.0],
+    },
+    Vertex {
+        // vertex d
+        position: [-0.5, 0.5],
+        color: [1.0, 1.0, 0.0],
+    },
+    Vertex {
+        // vertex d
+        position: [-0.5, 0.5],
+        color: [1.0, 1.0, 0.0],
+    },
+    Vertex {
+        // vertex b
+        position: [0.5, -0.5],
+        color: [0.0, 1.0, 0.0],
+    },
+    Vertex {
+        // vertex c
+        position: [0.5, 0.5],
+        color: [0.0, 0.0, 1.0],
+    },
+
+];
+
+//pack all fields required for drawing graphics into another structure called state
+struct State {
+
+    instance: wgpu::Instance,
+    surface: wgpu::Surface,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    size: winit::dpi::PhysicalSize<u32>,
+    pipeline: wgpu::RenderPipeline,
+    vertex_buffer: wgpu::Buffer,
+}
+
+impl State {
+    async fn new(window: &Window) -> Self {//contain code for creating adapter, device and basic configuration shader stuff and pipeline layout similar with previous examples except line 131
+        let size = window.inner_size();
+        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::DX12,
+            dx12_shader_compiler: Default::default(),
+        });
+        let surface = unsafe { instance.create_surface(window) }.unwrap();
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+                force_fallback_adapter:false
+            })
+            .await
+            .unwrap();
+
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    label: None,
+                    features: wgpu::Features::empty(),
+                    limits: wgpu::Limits::default(),
+                },
+                None, // Trace path
+            )
+            .await
+            .unwrap();
+
+        let surface_caps = surface.get_capabilities(&adapter);
+        let format = surface_caps.formats[0];
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format,
+            width: size.width,
+            height: size.height,
+            present_mode: wgpu::PresentMode::Fifo,
+            alpha_mode:surface_caps.alpha_modes[0],
+            view_formats: vec![],
+        };
+        surface.configure(&device, &config);
+
+        let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+            label: Some("Shader"),
+            source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
+        });
+
+        let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+            label: Some("Render Pipeline Layout"),
+            bind_group_layouts: &[],
+            push_constant_ranges: &[],
+        });
+        //to render the pipeline for the buffer field
+        let pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("Render Pipeline"),
+            layout: Some(&pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &shader,
+                entry_point: "vs_main",
+                buffers: &[Vertex::desc()],// the vertex description defined on line 22
+            },
+            fragment: Some(wgpu::FragmentState {
+                module: &shader,
+                entry_point: "fs_main",
+                targets: &[Some(wgpu::ColorTargetState {
+                    format: config.format,
+                    blend: Some(wgpu::BlendState {
+                        color: wgpu::BlendComponent::REPLACE,
+                        alpha: wgpu::BlendComponent::REPLACE,
+                    }),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList,
+                strip_index_format: None,
+                ..Default::default()
+            },
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState::default(),
+            multiview: None,
+        });
+        // and vertex buffer which is used by the init to create buffer with the descriptor
+        let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Vertex Buffer"),//using these labels
+            contents: bytemuck::cast_slice(VERTICES),
+            usage: wgpu::BufferUsages::VERTEX,
+        });
+
+        Self {
+            instance,
+            surface,
+            device,
+            queue,
+            config,
+            size,
+            pipeline,
+            vertex_buffer,
+        }
+    }
+
+    pub fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        if new_size.width > 0 && new_size.height > 0 {
+            self.instance.poll_all(true);
+            self.size = new_size;
+            self.config.width = new_size.width;
+            self.config.height = new_size.height;
+            self.surface.configure(&self.device, &self.config);
+        }
+    }
+
+    #[allow(unused_variables)]//input function returns a boolean result to indicate if an event has been processed
+    fn input(&mut self, event: &WindowEvent) -> bool {
+        false
+    }
+
+    fn update(&mut self) {}//update soon
+
+    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {//defines render paths
+        //let output = self.surface.get_current_frame()?.output;
+        let output = self.surface.get_current_texture()?;
+        let view = output
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Render Encoder"),
+            });
+
+        {
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.2,
+                            g: 0.247,
+                            b: 0.314,
+                            a: 1.0,
+                        }),
+                        store: true,
+                    },
+                })],
+                depth_stencil_attachment: None,
+            });
+
+            render_pass.set_pipeline(&self.pipeline);//set pipeline and vertex buffer to the render pass
+            render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+            render_pass.draw(0..6, 0..1);
+        }
+
+        self.queue.submit(iter::once(encoder.finish()));
+        output.present();
+
+        Ok(())
+    }
 }
 
 fn main() {
-    //========================Check backend EP1====================
-    //let instances = wgpu::Instance::new(wgpu::Backends::all());
-    //for adapter in instances.enumerate_adapters(wgpu::Backends::all()){
-    //     println!("{:?}",adapter.get_info())
-    // }
-    //====================Window Generation EP2=================
-    //let event_loop: EventLoop<()> = EventLoop::new();
-    //let window: Window = Window::new(&event_loop).unwrap();
-    //window.set_title("my_window");
-    //env_logger::init();
-    //event_loop.run(move |event: Event<()>, _,control_flow: &mut ControlFlow|{
-    //   *control_flow = ControlFlow::Wait;
-    //    match event {
-    //        Event::WindowEvent {
-    //            event: WindowEvent::CloseRequested,
-    //            ..
-    //       } => *control_flow = ControlFlow::Exit,
-    //        _=>{}
-    //    }
-    //});
-    let event_loop = EventLoop::new();
-    let window = winit::window::Window::new(&event_loop).unwrap();
-    window.set_title("wgpu03");
     env_logger::init();
-    pollster::block_on( run(event_loop, &window));
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+    window.set_title(&*format!("{}", "ch04-square"));
+    let mut state = pollster::block_on(State::new(&window));
+
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::WindowEvent {
+            ref event,
+            window_id,
+        } if window_id == window.id() => {
+            if !state.input(event) {
+                match event {
+                    WindowEvent::CloseRequested
+                    | WindowEvent::KeyboardInput {
+                        input:
+                        KeyboardInput {
+                            state: ElementState::Pressed,
+                            virtual_keycode: Some(VirtualKeyCode::Escape),
+                            ..
+                        },
+                        ..
+                    } => *control_flow = ControlFlow::Exit,
+                    WindowEvent::Resized(physical_size) => {
+                        state.resize(*physical_size);
+                    }
+                    WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                        state.resize(**new_inner_size);
+                    }
+                    _ => {}
+                }
+            }
+        }
+        Event::RedrawRequested(_) => {
+            state.update();
+            match state.render() {//call render function
+                Ok(_) => {}
+                Err(wgpu::SurfaceError::Lost) => state.resize(state.size),
+                Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                Err(e) => eprintln!("{:?}", e),
+            }
+        }
+        Event::MainEventsCleared => {
+            window.request_redraw();
+        }
+        _ => {}
+    });
 }
--- src/shader.wgsl
+++ src/shader.wgsl
@@ -1,28 +1,25 @@
-struct VOutput{
-    @location(0) v_color: vec4<f32>,
+struct VertexInput {
+    @location(0) pos: vec2<f32>,//position input at location 0 vec2 x,y
+    @location(1) color: vec3<f32>,//color input at location 1 vec3 r,g,b
+    //must be consistant with the shader location define rust code
+    //set relationship between shader and buffer
+};
+//havent defined any vertices and colors we will store that data in gpu buffers
+
+struct VertexOutput {//output for pos and color
     @builtin(position) position: vec4<f32>,
+    @location(0) color: vec4<f32>,
 };
 
 @vertex
-fn vs_main(@builtin(vertex_index) in_vertex_index: u32) -> VOutput {
-    var pos = array<vec2<f32>,3>(
-        vec2<f32>(0.0, 0.5),
-        vec2<f32>(-0.5,-0.5),
-        vec2<f32>(0.5,-0.5)
-    );
-    var color = array<vec3<f32>,3>(
-        vec3<f32>(1.0, 0.0, 0.0),
-        vec3<f32>(0.0, 1.0, 0.0),
-        vec3<f32>(0.0, 0.0, 1.0)
-    );
-
-    var out: VOutput;
-    out.position = vec4<f32>(pos[in_vertex_index], 0.0, 1.0);
-    out.v_color = vec4<f32>(color[in_vertex_index], 1.0);
+fn vs_main(in: VertexInput) -> VertexOutput {
+    var out: VertexOutput;
+    out.color = vec4<f32>(in.color, 1.0);//process color data
+    out.position = vec4<f32>(in.pos, 0.0, 1.0);//process position data to vec 4
     return out;
 }
 
 @fragment
-fn fs_main(in: VOutput) -> @location(0) vec4<f32> {
-    return in.v_color;
+fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
+    return in.color;
 }
