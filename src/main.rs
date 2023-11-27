fb7ef5a732d15e9cc33a56cd188c2261ac712731
Nickled2002 <64671430+Nickled2002@users.noreply.github.com>
2023-11-27 19:06:39 +0000

    Math Function to gaussian dist to resemble peaks colormap made to look like a mountain

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ mod common;
 mod math_func;
 
 fn main(){
-    let mut colormap_name = "jet";//color map name
-    let mut is_two_side:i32 = 0;//one sided
+    let mut colormap_name = "mountain";//color map name
+    let mut is_two_side:i32 = 0;//one sided no lighting on the under side
     let args: Vec<String> = std::env::args().collect();
     if args.len() > 1 {
         colormap_name = &args[1];
@@ -12,8 +12,8 @@ fn main(){
         is_two_side = args[2].parse().unwrap();
     }
     //create vertex data from common rs file and using the function from mathfunc.rs file
-    let vertex_data = common::create_vertices(&math_func::sinc, colormap_name, -8.0, 8.0, -8.0, 8.0,
-                                              30, 30, 2.0, 0.7);//set scale to two and aspect ratio to .7
+    let vertex_data = common::create_vertices(&math_func::peaks, colormap_name, -3.0, 3.0, -3.0, 3.0,
+                                              30, 30, 2.0, 0.9);//set scale to two and aspect ratio to .7 to .9 for more precise
     let light_data = common::light([1.0, 1.0, 1.0], 0.1, 0.8, 0.4, 30.0, is_two_side);//1,1,1 for specular light color and set light intensity
-    common::run(&vertex_data, light_data, colormap_name, "sinc");//create sinc surface
+    common::run(&vertex_data, light_data, colormap_name, "Peaks");//create sinc surface now peaks
 }
--- src/math_func.rs
+++ src/math_func.rs
@@ -1,4 +1,10 @@
 #![allow(dead_code)]
+pub fn peaks (x:f32, z:f32) -> [f32; 3] {
+    //new two input function finding y by translating and scaling Gaussian distribution will not be required with geographicaldata since XYZ will be given
+    let y = 3.0*(1.0-x)*(1.0-x)*(-(x*x)-(z+1.0)*(z+1.0)).exp()-
+        10.0*(x/5.0-x*x*x-z*z*z*z*z)*(-x*x-z*z).exp() - 1.0/3.0*(-(x+1.0)*(x+1.0)-z*z).exp();
+    [x, y, z]
+}
 pub fn sinc (x:f32, z:f32) -> [f32; 3] {
     //sinc function takes xz and calcs y so outputs xyz
     let r = (x*x + z*z).sqrt();
